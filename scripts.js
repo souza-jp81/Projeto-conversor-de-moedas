8a360fbc25d46bac1e50663e4d7fa65f2e0b4921
souza-jp81 <jpcsonline@gmail.com>
2025-06-06 14:29:32 -0300

    currency pound added

--- scripts.js
+++ scripts.js
@@ -1,14 +1,17 @@
 const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
+const currencyToConvert = document.querySelector(".currency-to-convert")
 
 function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
     const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
     const currencyValueConverted = document.querySelector(".currency-value")
 
-     
+
     const dolarToday = 5.64
     const euroToday = 6.38
+    const poundToday = 7.57
+    const realToday = 
 
     if (currencySelect.value == "dolar") {
         currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
@@ -24,6 +27,13 @@ function convertValues() {
         }).format(inputCurrencyValue / euroToday)
     }
 
+    if (currencySelect.value == "pound") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("en-GB", {
+            style: "currency",
+            currency: "GBP",
+        }).format(inputCurrencyValue / poundToday)
+    }
+
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
         style: "currency",
         currency: "BRL"
@@ -32,22 +42,30 @@ function convertValues() {
 }
 
 function changeCurrency() {
- const currencyName = document.getElementById("currency-name")
- const currencyImage = document.querySelector(".currency-img")
+    const currencyName = document.getElementById("currency-name")
+    const currencyImage = document.querySelector(".currency-img")
 
- if (currencySelect.value == "dolar") {
-    currencyName.innerHTML = "Dólar Americano"
-    currencyImage.src = "./assets/dolar.png"
- }
+    if (currencySelect.value == "dolar") {
+        currencyName.innerHTML = "Dólar Americano"
+        currencyImage.src = "./assets/dolar.png"
+    }
 
- if (currencySelect.value == "euro") {
-    currencyName.innerHTML = "Euro"
-    currencyImage.src="./assets/euro.png"
- }
+    if (currencySelect.value == "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImage.src = "./assets/euro.png"
+    }
 
- convertValues() 
+    if (currencySelect.value == "pound"){
+        currencyName.innerHTML = "Libra"
+        currencyImage.src = "./assets/pound.png"
+    }
+
+    convertValues()
 }
+
+function changeCurrencyBack(){}
 
 
 convertButton.addEventListener("click", convertValues)
 currencySelect.addEventListener("change", changeCurrency)
+currencyToConvert.addEventListener("change", changeCurrencyBack)
